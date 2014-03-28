5cd87b1416037ea96515e2675b20dc3822c93763
Neels Hofmeyr <neels@hofmeyr.de>
2014-03-28 03:02:26 +0100

    Add terminal colors, tweak output.

--- 0xb.py
+++ 0xb.py
@@ -1,13 +1,131 @@
 #!/usr/bin/env python
+
+# SNIP: included termcolor.py from http://pypi.python.org/pypi/termcolor/1.0.0
+########################################################################
+# Copyright (c) 2008-2011 Volvox Development Team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Author: Konstantin Lepa <konstantin.lepa@gmail.com>
+
+"""ANSII Color formatting for output in terminal."""
+
+import os
+
+
+__ALL__ = [ 'colored' ]
+
+VERSION = (1, 0, 0)
+
+ATTRIBUTES = dict(
+        list(zip([
+            'bold',
+            'dark',
+            '',
+            'underline',
+            'blink',
+            '',
+            'reverse',
+            'concealed'
+            ],
+            list(range(1, 9))
+            ))
+        )
+del ATTRIBUTES['']
+
+
+HIGHLIGHTS = dict(
+        list(zip([
+            'on_grey',
+            'on_red',
+            'on_green',
+            'on_yellow',
+            'on_blue',
+            'on_magenta',
+            'on_cyan',
+            'on_white'
+            ],
+            list(range(40, 48))
+            ))
+        )
+
+
+COLORS = dict(
+        list(zip([
+            'grey',
+            'red',
+            'green',
+            'yellow',
+            'blue',
+            'magenta',
+            'cyan',
+            'white',
+            ],
+            list(range(30, 38))
+            ))
+        )
+
+
+RESET = '\033[0m'
+
+
+def colored(text, color=None, on_color=None, attrs=None):
+    """Colorize text.
+
+    Available text colors:
+        red, green, yellow, blue, magenta, cyan, white.
+
+    Available text highlights:
+        on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white.
+
+    Available attributes:
+        bold, dark, underline, blink, reverse, concealed.
+
+    Example:
+        colored('Hello, World!', 'red', 'on_grey', ['blue', 'blink'])
+        colored('Hello, World!', 'green')
+    """
+    if not text:
+      return text
+    if os.getenv('ANSI_COLORS_DISABLED') is None:
+        fmt_str = '\033[%dm%s'
+        if color is not None:
+            text = fmt_str % (COLORS[color], text)
+
+        if on_color is not None:
+            text = fmt_str % (HIGHLIGHTS[on_color], text)
+
+        if attrs is not None:
+            for attr in attrs:
+                text = fmt_str % (ATTRIBUTES[attr], text)
+
+        text += RESET
+    return text
+
+########################################################################
+# SNIP: end of termcolor.py from http://pypi.python.org/pypi/termcolor/1.0.0
+# GPLv3 License applies from this line on.
+
 import sys, random, time
 
-def val2char(val):
-  if not val:
-    return '.'
-  else:
-    return hex(val)[2:]
 
-def shove(items, reverse=False):
+def shove(items, zero_items=lambda l:[0]*l, reverse=False):
   was_len = len(items)
   if reverse:
     items = reversed(items)
@@ -15,62 +133,107 @@ def shove(items, reverse=False):
   i = 0
   while i < (len(items) - 1):
     if items[i] == items[i + 1]:
-      items[i] += 1
-      del items[i+1]
+      items[i + 1] += 1
+      del items[i]
     i += 1
-  items.extend([0] * (was_len - len(items)))
+  items.extend(zero_items(was_len - len(items)))
   if reverse:
     items = list(reversed(items))
   return items
 
+
+class Cell:
+  last_cell_color = 0
+  colors = ('white', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', )
+
+  def __init__(c, val=0, col=None):
+    c.val = val
+    if col is None:
+      Cell.last_cell_color += 1
+      col = Cell.last_cell_color
+    c.col = col
+
+  def __cmp__(c, other):
+    return cmp(c.val, int(other))
+
+  def __repr__(c):
+    return '<%d,%d>'%(c.val, c.col)
+
+  def __nonzero__(c):
+    return bool(c.val)
+
+  def __int__(c):
+    return c.val
+
+  def __iadd__(c, other):
+    c.val += int(other)
+    return c
+
+  def __eq__(c, other):
+    if other is None:
+      return False
+    return c.val == int(other)
+
+  def __str__(c):
+    if not c.val:
+      return '.'
+    else:
+      s = hex(c.val)[2:]
+      return colored(s, Cell.colors[c.col % len(Cell.colors)])
+
+
+def new_cells(n):
+  c = []
+  for i in range(n):
+    c.append(Cell())
+  return c
 
 class Board:
 
   def __init__(b, w=4, h=4):
     b.W = w
     b.H = h
-    b.rows = [[0] * w] * h
-    b.rows = [list(row) for row in b.rows] # make sure each is a single list
-
-  def cell_char(b, x, y):
-    return val2char(b.rows[y][x])
+    b.rows = []
+    for y in range(h):
+      b.rows.append(new_cells(w))
 
   def cell(b, x, y):
     return b.rows[y][x]
 
   def draw(b):
     for r in b.rows:
-      print ''.join([val2char(v) for v in r])
+      print ''.join([str(c) for c in r])
 
   def left(b):
     for r in range(len(b.rows)):
-      b.rows[r] = shove(b.rows[r])
+      b.rows[r] = shove(b.rows[r], new_cells)
 
   def right(b):
     for r in range(len(b.rows)):
-      b.rows[r] = shove(b.rows[r], True)
+      b.rows[r] = shove(b.rows[r], new_cells, True)
 
   def up(b):
     cols = [list(col) for col in zip(*b.rows)]
     for c in range(len(cols)):
-      cols[c] = shove(cols[c])
+      cols[c] = shove(cols[c], new_cells)
     b.rows = [list(row) for row in zip(*cols)]
 
   def down(b):
     cols = [list(col) for col in zip(*b.rows)]
     for c in range(len(cols)):
-      cols[c] = shove(cols[c], True)
+      cols[c] = shove(cols[c], new_cells, True)
     b.rows = [list(row) for row in zip(*cols)]
 
   def drop(b):
     coords = []
     for y in range(b.H):
       for x in range(b.W):
+        c = b.cell(x,y)
         if not b.cell(x, y):
           coords.append((x, y))
     if coords:
       x, y = random.choice(coords)
-      b.rows[y][x] = random.choice((1, 2))
+      b.rows[y][x].val = random.choice((1, 2))
       return True
     return False
 
@@ -133,7 +296,7 @@ if __name__ == '__main__':
         if b.rows != old_rows:
           b.drop()
       else:
-        print 'use arrow keys!'
+        print 'use arrow keys to shove, ctrl-c to quit'
   finally:
       termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
       fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
